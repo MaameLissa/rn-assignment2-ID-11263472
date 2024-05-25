245144020790e86c8e20d591ce60397bfd0b6d36
MaameLissa <motoo110@st.ug.edu.gh>
2024-05-25 00:34:49 +0000

    Name

--- Muse/App.js
+++ Muse/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>!</Text>
+      <Text> My name is Melissa Otoo. </Text>
       <StatusBar style="auto" />
     </View>
   );
