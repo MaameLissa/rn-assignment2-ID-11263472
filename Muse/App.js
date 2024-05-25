2eaa096b3132f777318fd9e952eda883f4b3c3db
MaameLissa <motoo110@st.ug.edu.gh>
2024-05-25 05:50:15 +0000

    FontSize and Bold

--- Muse/App.js
+++ Muse/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> My name is Melissa Otoo. </Text>
+      <Text style = {styles.text}>My name is<Text style={styles.boldText}> Melissa Otoo</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 24,
+    },
+    boldText:{
+    fontWeight: 'bold'
+    },
+
 });
